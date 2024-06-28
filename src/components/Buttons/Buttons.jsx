36f96fa5b73ae8d7101e5fd174d2c695f42b21af
cravsky <cravsky@gmail.com>
2024-06-28 15:09:06 +0200

    Checked css module

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,9 +1,11 @@
+import classes from './Buttons.module.css';
+
 export default function Buttons({ selectQuote, tweetQuote, quotes }) {
 
-    return (<div className="button-container">
-        <button className="twitter-button" title="Tweet this!"
+    return (<div className={classes['button-container']}>
+        <button className={classes['twitter-button']} title="Tweet this!"
             onClick={() => tweetQuote()}>
-            <i className="fab fa-twitter"></i>
+            <i className='fab fa-twitter'></i>
         </button>
         <button onClick={() => selectQuote(quotes)}>New Quote</button>
     </div>)
